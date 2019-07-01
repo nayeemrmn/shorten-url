fb92577c441dd6953152ac6f95f3925e55c92abe
Nayeem <muhammed.9939@gmail.com>
2019-07-01 16:49:07 +0100

    Reset the path index when removing all short URL's

--- app/models/shortURL/remove-all.js
+++ app/models/shortURL/remove-all.js
@@ -4,8 +4,15 @@ import databaseConnect from '../../database/connect.js';
 /* Remove all short URL entries. */
 export default async () => {
   return databaseConnect().then(database => {
-    return database.deleteAll(COLLECTIONS.SHORT_URLS).catch(error => {
-      return Promise.reject(`Couldn't remove all short URL data: ${error}`);
+    return database.pullGlobals().then(globals => {
+      globals.nextIndex = 0;
+      return database.pushGlobals(globals);
+    }).catch(error => {
+      return Promise.reject(`Couldn't reset the index: ${error}`);
+    }).then(() => {
+      return database.deleteAll(COLLECTIONS.SHORT_URLS).catch(error => {
+        return Promise.reject(`Couldn't remove all short URL data: ${error}`);
+      });
     });
   });
 };
