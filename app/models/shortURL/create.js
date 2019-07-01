b4539ca0edce5e9a81a0a160deca82bbc91cd6af
Nayeem <muhammed.9939@gmail.com>
2019-07-01 01:45:16 +0100

    Use a path blacklist instead of a base index to avoid collisions with 'api' etc.

--- app/models/shortURL/create.js
+++ app/models/shortURL/create.js
@@ -1,7 +1,9 @@
 import COLLECTIONS from '../../database/collections.js';
 import databaseConnect from '../../database/connect.js';
 
-const BASE_INDEX = 13879;
+const UNUSABLE_PATHS = Object.freeze([
+  'api'
+]);
 
 /* Create a short URL entry and resolve with an (ID, shortURL) tuple. */
 export default async destination => {
@@ -10,10 +12,14 @@ export default async destination => {
       return Promise.reject(`Couldn't get the next index: ${error}`);
     }).then(globals => {
       if (!globals.nextIndex) {
-        globals.nextIndex = BASE_INDEX;
+        globals.nextIndex = 0;
       }
-      const shortURL = {destination, path: globals.nextIndex.toString(36)};
-      globals.nextIndex++;
+      let path;
+      do {
+        path = globals.nextIndex.toString(36);
+        globals.nextIndex++;
+      } while (UNUSABLE_PATHS.includes(path));
+      const shortURL = {destination, path};
       return database.pushGlobals(globals).catch(error => {
         return Promise.reject(`Failed to commit the next index: ${error}`);
       }).then(() => {
